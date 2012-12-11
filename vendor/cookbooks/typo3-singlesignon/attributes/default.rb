5d62f7531740c19cfd32cbfbc24280990531731e
Christopher Hlubek <hlubek@networkteam.com>
2012-12-11 18:50:35 +0100

    [TASK] Use Production context and tune services for VM

--- vendor/cookbooks/typo3-singlesignon/attributes/default.rb
+++ vendor/cookbooks/typo3-singlesignon/attributes/default.rb
@@ -1,5 +1,18 @@
 # default['kibana']['home'] = "/home/kibana"
 
 
-include_attribute "php"
-# node.set['php']['directives'] = node['php']['directives'].merge({'date.timezone' => 'Europe/Berlin'})
+# More lightweight apache setup for the demo
+include_attribute "apache2"
+node.set['apache']['prefork']['startservers'] = 2
+node.set['apache']['prefork']['minspareservers'] = 3
+node.set['apache']['prefork']['maxspareservers'] = 3
+node.set['apache']['prefork']['serverlimit'] = 75
+node.set['apache']['prefork']['maxclients'] = 75
+
+# More lightweight mysql setup for the demo
+include_attribute "mysql::server"
+node.set['mysql']['tunable']['key_buffer'] = "32M"
+node.set['mysql']['tunable']['max_heap_table_size'] = "16M"
+node.set['mysql']['tunable']['innodb_buffer_pool_size'] = "32M"
+node.set['mysql']['tunable']['innodb_additional_mem_pool_size'] = "1M"
+node.set['mysql']['tunable']['query_cache_size'] = "4M"
